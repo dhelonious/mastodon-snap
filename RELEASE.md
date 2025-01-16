64a723eda6d091a4c3f1a2b8ba0c62dc7d5603dd
Daniel Dizdarevic <daniel.dizdarevic@proton.me>
2025-01-16 13:44:19 +0100

    Upgrade to Mastodon 4.3.3

--- RELEASE.md
+++ RELEASE.md
@@ -1,6 +1,6 @@
-# Unofficial snap for Mastodon 4.3.2
+# Unofficial snap for Mastodon 4.3.3
 
-See the release notes for Mastodon: https://github.com/mastodon/mastodon/releases/tag/v4.3.2
+See the release notes for Mastodon: https://github.com/mastodon/mastodon/releases/tag/v4.3.3
 
 This release also includes:
 
@@ -11,31 +11,3 @@ This release also includes:
 * redis 7.4.2
 * libvips 8.16.0
 * ffmpeg 7.1
-
-## Changelog for 4.3.2snap6:
-
-* Hotfix for `media.dir` setting being empty on install
-* Add spinning to confetti animation
-* Upgrade to redis 7.4.2
-* Hotfix for `mastodon-server.get-certificate` `acme.server` check [4.3.2snap4]
-* It's your Fediday: The day you joined the Fediverse will be celebrated by your instance with a confetti rain! 🎊 [4.3.2snap4]
-* Support [BuyPass](https://buypass.com) SSL CA [4.3.2snap4]
-* Yarn updated to 4.6.0 [4.3.2snap4]
-* Let it snow: Around Christmas, snow will start falling in your Mastodon instance! ❄️ [4.3.2snap3]
-* Adds snow to the Mastodon UI between Christmas and New Year! (Thanks to [Roni Laukkarinen](https://github.com/ronilaukkarinen)!) [4.3.2snap3]
-* Admin account created during setup [4.3.2snap3]
-* Removed the `status.char-counter` option and renamed `status.char-limit` to `status.length [4.3.2snap3]
-* Additional improvements have been made to the `mastodon-server.setup` and `mastodon-server.get-certificate` scripts [4.3.2snap3]
-* Mastodon Bird UI updated to 2.1.1 [4.3.2snap3]
-
-## Upgrade notes
-
-Due to the changes described above, you may need to manually change the `status.length` option after the upgrade, e.g:
-
-    snap set mastodon-server status.length=5000
-
-However, the new default value of `status.length=1000` doubles Mastodon's default value of 500 and should be sufficient for most users.
-
-You can also clean up the old snap variables with:
-
-    snap unset mastodon-server status.char-limit status.char-counter
--- tools/get_new_versions.py
+++ tools/get_new_versions.py
@@ -13,15 +13,26 @@ import requests
 parser = argparse.ArgumentParser(description="Get versions of dependencies")
 parser.add_argument("mastodon_version", nargs="?")
 parser.add_argument("-a", "--all", help="Show all versions, even if they are not out of date", action="store_true")
+parser.add_argument("-v", "--verbose", help="Show additional ouptut", action="store_true")
 args = parser.parse_args()
 
+def print_verbose(*line):
+    if args.verbose:
+        print(*line)
+
 if args.mastodon_version:
+    print_verbose("Using the given Mastodon release...")
     MASTODON_RELEASE = f"v{ args.mastodon_version }"
-    print(f"Use Mastodon release: {MASTODON_RELEASE}")
 else:
+    print_verbose("Getting the latest Mastodon release...")
     with requests.get("https://github.com/mastodon/mastodon/releases/latest", timeout=10) as r:
-        MASTODON_RELEASE = r.url.rstrip("/").split("/")[-1]
-    print(f"Use latest Mastodon release: {MASTODON_RELEASE}")
+        if r.status_code != 200:
+            print(r.raise_for_status())
+            exit()
+        else:
+            MASTODON_RELEASE = r.url.rstrip("/").split("/")[-1]
+
+print(f"Use Mastodon release: {MASTODON_RELEASE}")
 
 with requests.get(f"https://raw.githubusercontent.com/mastodon/mastodon/{ MASTODON_RELEASE }/Vagrantfile", timeout=10) as r:
     NODE_MAJOR = re.findall(r".*NODE_MAJOR=([0-9]+).*", r.text)[0]
@@ -110,8 +121,8 @@ def print_table_header():
     print(f"{'Name':<15} {'Local version':<15} {'New version':<15} SHA256 Checksum")
 
 def print_table(name, local_version, new_version, checksum=""):
-    if version != local_version or args.all:
-        if version != local_version and args.all:
+    if local_version != new_version or args.all:
+        if local_version != new_version and args.all:
             name += "*"
         print(f"{name:<15} {local_version:<15} {new_version:<15} {checksum}")
 
@@ -132,17 +143,26 @@ def minor(version):
     return ".".join(version.split(".")[0:1])
 
 
-print_table_header()
-
+table = []
 for name, settings in dependencies.items():
-    r = requests.get(settings["url"], timeout=10)
-    version = None
+    print(f"Cecking {name}...")
 
+    r = requests.get(settings["url"], timeout=10)
+    print_verbose(f"{settings['url']} -> {r.status_code}")
+
+    if r.status_code != 200:
+        print(r.raise_for_status())
+        continue
+
+    version = None
     if settings["url"].endswith("latest"):
+        print_verbose("Using 'latest' URL...")
         version = r.url.rstrip("/").split("/")[-1]
 
     elif settings["url"].startswith("https://api.github.com"):
+        print_verbose("Use GitHub API...")
         tags = [d["name"] for d in r.json()]
+        print_verbose(f"Tags found: {tags}")
         if "exclude" in settings:
             tags = [t for t in tags if settings["exclude"] not in t]
         if "regex" in settings:
@@ -152,25 +172,49 @@ for name, settings in dependencies.items():
 
     else:
         if "regex" in settings:
+            print_verbose("Using regex on page content...")
             version = settings["regex"].findall(r.text)[0]
         else:
+            print_verbose("Using raw page content...")
             version = r.text.strip()
 
     if "lstrip" in settings:
+        print_verbose("Applying lstrip...")
         version = version.lstrip(settings["lstrip"])
     if "replace" in settings:
+        print_verbose("Applying replace...")
         version = version.replace(*settings["replace"])
+    print_verbose(f"-> {version}")
 
+    print_verbose("Getting local version...")
     local_source = re.findall(settings["local_regex"], snapcraft_yaml)[0]
     if isinstance(local_source, str):
         local_version = local_source
+        print_verbose(f"-> {local_version}")
+        print_verbose("Checksum calculation not required")
         checksum = ""
     else:
         local_version = local_source[1]
-        url = local_source[0]
-        url = url.replace(local_version, version)
-        url = url.replace(minor(local_version), minor(version))
-        url = url.replace(major(local_version), major(version))
-        checksum = sha256_checksum(url)
+        print_verbose(f"-> {local_version}")
+        if version != local_version:
+            url = local_source[0]
+            url = url.replace(local_version, version)
+            url = url.replace(minor(local_version), minor(version))
+            url = url.replace(major(local_version), major(version))
+            print_verbose(f"Checksum URL: {url}")
+            print_verbose("Calculating checksum...")
+            checksum = sha256_checksum(url)
+            print_verbose(f"-> {checksum}")
+        else:
+            print_verbose("Checksum calculation not required")
+            checksum = ""
 
-    print_table(name, local_version, version, checksum)
+    if local_version != version or args.all:
+        table.append((name, local_version, version, checksum))
+
+if table:
+    print()
+    print_verbose("Printing table...")
+    print_table_header()
+    for entry in table:
+        print_table(*entry)
