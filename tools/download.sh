d0505fc1b4a118c809218cb2d4db5039168a7d35
Daniel Dizdarevic <daniel.dizdarevic@proton.me>
2026-01-01 21:10:37 +0100

    Update artifact download script
    The script will only download the latest snap artifact if other
    artifacts have been built.

--- tools/download.sh
+++ tools/download.sh
@@ -3,7 +3,11 @@
 
 REPO="dhelonious/mastodon-snap"
 ACCESS_TOKEN="github_pat_XXXXXX" # Replace this with your personal access token
-LATEST_ARTIFACT=$(curl -sL -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $ACCESS_TOKEN" "https://api.github.com/repos/$REPO/actions/artifacts" | jq -r ".artifacts[0].archive_download_url")
+LATEST_ARTIFACT=$(curl -sL \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $ACCESS_TOKEN" \
+  "https://api.github.com/repos/$REPO/actions/artifacts" |
+jq -r '.artifacts | map(select(.name | endswith(".snap"))) | sort_by(.created_at) | last | .archive_download_url')
 
 echo Downloading latest snap from $REPO
 curl -LH "Authorization: Bearer $ACCESS_TOKEN" --progress-bar "$LATEST_ARTIFACT" -o /tmp/snap.zip
